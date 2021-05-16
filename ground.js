f13c37639dfc2161cd9030a2a7b06d4a0058b6a0
minttech <84032482+minttech@users.noreply.github.com>
2021-05-16 12:03:26 +0530

    Add files via upload

---
+++ ground.js
@@ -0,0 +1,21 @@
+class Ground{
+    constructor(x, y, width, height)
+    {
+        var opt={
+        isStatic:true
+        
+
+         }     
+     this.body= Bodies.rectangle(x, y, width, height,opt)
+     this.w=width;
+     this.h=height;
+     World.add(world, this.body) 
+    }
+display()
+{
+   var pos= this.body.position;
+   rectMode(CENTER);
+   rect(pos.x,pos.y,this.w,this.h)
+}
+
+}
